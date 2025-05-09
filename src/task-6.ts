ee297ff3d4bde50ff9cf7b5f2ab1a6c81002fb9a
Svitlana <missis.chuvaeva@ukr.net>
2025-05-09 23:06:39 +0300

    add changes

--- src/task-6.ts
+++ src/task-6.ts
@@ -13,6 +13,6 @@ function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
 
-getFirstElement([1, 2, 3]);           // 1
-getFirstElement(["a", "b", "c"]);     // "a"
-getFirstElement([true, false, true]); // true
+console.log(getFirstElement([1, 2, 3]));           // 1
+console.log(getFirstElement(["a", "b", "c"]));     // "a"
+console.log(getFirstElement([true, false, true])); // true
